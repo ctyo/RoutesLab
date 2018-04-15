525908b3f8a943bf021837d8f128bd35c77b64b6
ctyo <ctyo@live.jp>
2018-04-15 19:26:11 +0900

    パラメータなし時の処理追加

--- routeslab.js
+++ routeslab.js
@@ -23,6 +23,23 @@ document.title = title;
 var id_list = url.searchParams.get('id') ? url.searchParams.get('id').split(',') : [];
 var layers = new Map();
 
+if (id_list.length <= 0) {
+  if (title === 'RoutesLab ルーツラボ') {
+    if(ret = window.prompt('タイトルを入力してください', '')){
+      title = ret;
+    }
+  }
+
+  ret = window.prompt('表示したいルートラボのIDをカンマ区切りで入力してください。\n画面に遷移します。', '')
+  if (ret) {
+    window.location.href = '?title='+title+'&id='+ret;
+  } else {
+    alert ('サンプルの地図を表示します...');
+    window.location.href = '?title=湖一周×4&id=f613116caa1ddc2899da15fa933b0aac,3700de6613eaf7637e9bebdd50dc82f7,6f262dd88888240f30706b8c809bfc65';
+  }
+
+}
+
 
 id_list.forEach(function(id){
     var xmlurl = 'https://latlonglab.yahoo.co.jp/route/get?&format=kml&rd=2000&id=' + id;
